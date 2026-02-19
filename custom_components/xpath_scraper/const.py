75cdfff48fff1913aa59fdc09747db35700961f5
Mauro Midolo <mauro.midolo@hotmail.com>
2026-02-19 09:40:34 +0100

    Refactor configuration keys and add state class
    Added new configuration key for state class and updated comments for clarity.

--- custom_components/xpath_scraper/const.py
+++ custom_components/xpath_scraper/const.py
@@ -1,17 +1,19 @@
 DOMAIN = "xpath_scraper"
-
 SENSOR = "sensor"
 PLATFORMS = [SENSOR]
 
-#Configuration 
-CONF_ALIAS_VAR="xpath_scraper_name"
-CONF_URL_VAR="xpath_scraper_url"
-CONF_SLL_CHECK_VAR="xpath_scraper_ssl_check"
-CONF_XPATH_VAR="xpath_scraper_xpath"
-CONF_CLEAN_VAR="xpath_scraper_clean_check"
-CONF_UNIT_OF_MEASUREMENT_VAR="xpath_scraper_unit_of_measurement"
+# Configuration keys
+CONF_ALIAS_VAR = "xpath_scraper_name"
+CONF_URL_VAR = "xpath_scraper_url"
+CONF_SLL_CHECK_VAR = "xpath_scraper_ssl_check"
+CONF_XPATH_VAR = "xpath_scraper_xpath"
+CONF_CLEAN_VAR = "xpath_scraper_clean_check"
+CONF_UNIT_OF_MEASUREMENT_VAR = "xpath_scraper_unit_of_measurement"
 
-#Update Inteval
+# NEW: state_class config key
+CONF_STATE_CLASS_VAR = "xpath_scraper_state_class"
+
+# Update interval
 CONF_SCAN_INTERVAL = "scan_interval"
 DEFAULT_SCAN_INTERVAL = 60
 
@@ -19,37 +21,23 @@ from homeassistant.const import (
     UnitOfEnergy,
     UnitOfVolume,
     CURRENCY_EURO,
-    CURRENCY_DOLLAR
+    CURRENCY_DOLLAR,
 )
 from homeassistant.components.sensor import SensorDeviceClass
-# Dizionario con le informazioni per diverse unità di misura
+
+# Dictionary with info for different units of measurement
 units = {
-    "None": {
-        "device_class": None, 
-        "unit": None
-    },
-    "$": {
-        "device_class": SensorDeviceClass.MONETARY, 
-        "unit": CURRENCY_DOLLAR
-    },
-    "€": {
-        "device_class": SensorDeviceClass.MONETARY,
-        "unit": CURRENCY_EURO
-    },
+    "None": {"device_class": None, "unit": None},
+    "$": {"device_class": SensorDeviceClass.MONETARY, "unit": CURRENCY_DOLLAR},
+    "€": {"device_class": SensorDeviceClass.MONETARY, "unit": CURRENCY_EURO},
     "€/L": {
         "device_class": SensorDeviceClass.MONETARY,
-        "unit": f"{CURRENCY_EURO}/{UnitOfVolume.LITERS}"
+        "unit": f"{CURRENCY_EURO}/{UnitOfVolume.LITERS}",
     },
     "$/gal": {
         "device_class": SensorDeviceClass.MONETARY,
         "unit": f"{CURRENCY_DOLLAR}/{UnitOfVolume.GALLONS}",
     },
-    "L": {
-        "device_class": SensorDeviceClass.VOLUME,
-        "unit": UnitOfVolume.LITERS
-    },
-    "kWh": {
-        "device_class": SensorDeviceClass.ENERGY,
-        "unit": UnitOfEnergy.KILO_WATT_HOUR
-    }
+    "L": {"device_class": SensorDeviceClass.VOLUME, "unit": UnitOfVolume.LITERS},
+    "kWh": {"device_class": SensorDeviceClass.ENERGY, "unit": UnitOfEnergy.KILO_WATT_HOUR},
 }
